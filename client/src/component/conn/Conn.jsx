85654524ec8699ae027fe8586d8d640f0c9f28fd
guanhongjie <471671758@qq.com>
2019-11-04 21:39:29 +0800

    add antd

--- client/src/component/conn/Conn.jsx
+++ client/src/component/conn/Conn.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from "react"
+import { Form, Button, Input,Icon } from "antd"
+import "./Conn.css"
 
-class Conn extends Component {
+const { TextArea } = Input
+
+class ConnForm extends Component {
     constructor(props) {
         super(props)
         this.state = {}
@@ -10,18 +14,74 @@ class Conn extends Component {
     }
     connSocket = () => {
         let socket = new WebSocket("ws://localhost:9000")
-        console.log(socket)
         socket.onopen = event => {
-            console.log(event, "onopen")
             socket.send("hello webSocket")
         }
         socket.onmessage = function(event) {
             console.log(event.data)
         }
     }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log("Received values of form: ", values)
+            }
+        })
+    }
     render() {
-        return <div>Conn component</div>
+        const {getFieldDecorator} = this.props.form
+        return (
+            <div className="input-form">
+                <Form onSubmit={this.handleSubmit} className="form">
+                    <Form.Item>
+                        {getFieldDecorator("username", {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: "Please input your username!",
+                                },
+                            ],
+                        })(
+                            <Input
+                                prefix={
+                                    <Icon
+                                        type="user"
+                                        style={{ color: "rgba(0,0,0,.25)" }}
+                                    />
+                                }
+                                placeholder="Username"
+                            />
+                        )}
+                    </Form.Item>
+                    <Form.Item>
+                    {getFieldDecorator("username", {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: "Please input your username!",
+                                },
+                            ],
+                        })(
+                            <TextArea row={4} placeholder="discription"></TextArea>
+                        )}
+                        
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">
+                            submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+                <div className="show-box">
+                    <TextArea row={8}></TextArea>
+                </div>
+            </div>
+        )
     }
 }
+
+const Conn = Form.create({ name: 'conn' })(ConnForm);
 
 export { Conn }
